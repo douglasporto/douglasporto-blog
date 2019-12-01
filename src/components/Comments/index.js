0475374dc49979ee35b1181e45f3788005ba4bb4
Douglas Porto <douglasalexandre7@gmail.com>
2019-11-30 22:27:13 -0300

    Feat: Comments with disqus

---
+++ src/components/Comments/index.js
@@ -0,0 +1,28 @@
+import React from "react"
+import PropTypes from "prop-types"
+import ReactDisqusComments from "react-disqus-comments"
+
+import * as S from "./styles"
+
+const Comments = ({ url, title }) => {
+  const completeURL = `https://douglasporto.com.br/blog${url}`
+
+  return (
+    <S.CommentsWrapper>
+      <S.CommentsTitle>Comentários</S.CommentsTitle>
+      <ReactDisqusComments
+        shortname="douglas-porto"
+        identifier={completeURL}
+        title={title}
+        url={completeURL}
+      />
+    </S.CommentsWrapper>
+  )
+}
+
+Comments.propTypes = {
+  url: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+}
+
+export default Comments
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import { graphql } from "gatsby"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import RecommendedPosts from "../components/RecommendedPosts"
+import Comments from "../components/Comments"
 
 import * as S from "../components/Post/styles"
 
@@ -27,6 +28,7 @@ const BlogPost = ({ data, pageContext }) => {
         <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
       </S.MainContent>
       <RecommendedPosts next={next} previous={previous} />
+      <Comments url={post.fields.slug} title={post.frontmatter.title} />
     </Layout>
   )
 }
