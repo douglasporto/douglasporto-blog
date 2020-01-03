5257c35f4a49220009c0d76314babf01fe1ef179
Douglas Porto <douglasalexandre7@gmail.com>
2020-01-03 17:54:00 -0300

    Feat: Netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,6 +71,7 @@ const plugins = [
   // this (optional) plugin enables Progressive Web App + Offline functionality
   // To learn more, visit: https://gatsby.dev/offline
   `gatsby-plugin-offline`,
+  `gatsby-plugin-netlify-cms`,
 ]
 
 // if (process.env.CONTEXT === "production") {
