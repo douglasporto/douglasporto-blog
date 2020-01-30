b48ee8690b3dfb4c511acc680e6a465ed5b43775
Douglas Porto <douglasalexandre7@gmail.com>
2020-01-29 23:00:11 -0300

    Chore: new quey algolia

--- src/utils/algolia.js
+++ src/utils/algolia.js
@@ -22,6 +22,30 @@ const postQuery = `{
   }
 }`
 
+const dicQuery = `{
+  posts: allMarkdownRemark(
+    filter: { fileAbsolutePath: { regex: "/content/dicdevs/" } }
+  ) {
+    edges {
+      node {
+        objectID: id
+        fields {
+          slug
+        }
+        frontmatter {
+          date_timestamp: date
+          date(locale: "pt-br", formatString: "DD MMM[,] YYYY")
+          description
+          title
+          tags
+        }
+        timeToRead
+        excerpt(pruneLength: 5000)
+      }
+    }
+  }
+}`
+
 const flatten = arr => {
   return arr.map(({ node: { frontmatter, ...rest } }) => ({
     ...frontmatter,
@@ -36,6 +60,12 @@ const queries = [
     query: postQuery,
     transformer: ({ data }) => flatten(data.posts.edges),
     indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
+    settings,
+  },
+  {
+    query: dicQuery,
+    transformer: ({ data }) => flatten(data.posts.edges),
+    indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME_DICDEV,
     settings,
   },
 ]
