9d370c2e0747599385781ede9d2254fa0e7f1633
Douglas Porto <douglasalexandre7@gmail.com>
2020-08-16 16:46:52 -0300

    try fix rss

--- gatsby-config.js
+++ gatsby-config.js
@@ -138,8 +138,8 @@ const plugins = [
               return Object.assign({}, edge.node.frontmatter, {
                 description: edge.node.excerpt,
                 date: edge.node.frontmatter.date,
-                url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                url: site.siteMetadata.siteUrl + '/' + edge.node.fields.slug,
+                guid: site.siteMetadata.siteUrl + '/' + edge.node.fields.slug,
                 custom_elements: [{ "content:encoded": edge.node.html }],
               })
             })
