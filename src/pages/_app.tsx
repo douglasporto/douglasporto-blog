6db51c8f5e490f3d0eed6b7b3e88cbc20be41e59
douglasporto <14853660+douglasporto@users.noreply.github.com>
2022-02-11 11:37:47 -0300

    :wrench: Config(general): eslint, prettier & baseURL

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import { AppProps } from 'next/app'
+
 import { ChakraProvider } from '@chakra-ui/react'
-import { extendTheme } from '@chakra-ui/react'
+
 import theme from '../styles/theme'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-    return (
+  return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
